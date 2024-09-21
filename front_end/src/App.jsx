306f22c0cfb0a318d4bb4ee77f79804259be10fb
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-21 21:56:51 +0530

    commented react module

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import home from './pages/Home'
-import collection from './pages/Collection'
+//import React from 'react'
+//import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Routes>
-        <Route path='/'element={<home/>} />
-        <Route path='/collection'element={<collection/>} />
-
-
-      </Routes>
+    <div >
+      
     </div>
   )
 }
