9097d5a51d34613ab99f67f6c472498a0f303cb3
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-21 20:51:07 +0530

    import {Routes} from 'react-router-dom'

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {Routes} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+
+      </Routes>
     </div>
   )
 }
