37e2b72671208a453c6bb57134a090da3f39e3da
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-21 21:04:14 +0530

    change file name

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import {Routes} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
+import home from './pages/home'
+import collection from './pages/collection'
+
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Routes>
+        <Route path='/'element={<home/>} />
+        <Route path='/collection'element={<collection/>} />
+
 
       </Routes>
     </div>
