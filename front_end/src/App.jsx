605f6dc82231ac6c85adc3a7136aab8ae5c3ca4a
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-21 22:04:35 +0530

    create className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,9 +3,10 @@ import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div >
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Routes>
         
+
       </Routes>
       
     </div>
