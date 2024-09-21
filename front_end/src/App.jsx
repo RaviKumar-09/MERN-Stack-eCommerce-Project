43dbebe6d88fead7efffd18581af6d29c84a6e8f
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-21 21:58:07 +0530

    import {Routes,Route} from 'react-router-dom'

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,9 +1,12 @@
 //import React from 'react'
-//import {Routes,Route} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div >
+      <Routes>
+        
+      </Routes>
       
     </div>
   )
